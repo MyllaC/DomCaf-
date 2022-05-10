15e2038786852d114e707b4e7b26de925e589170
Camila Cristina <criscamila09@gmail.com>
2022-05-10 14:45:09 -0300

    carousel responsivo

--- main.js
+++ main.js
@@ -92,3 +92,13 @@ ScrollReveal({
   #about, 
   #about header, 
   #about .content`)
+
+
+// carousel
+document.querySelector('#items').addEventListener('wheel', event => {
+  if (event.deltaY > 0) {
+    event.target.scrollBy(300, 0)
+  } else {
+    event.target.scrollBy(-300, 0)
+  }
+})
